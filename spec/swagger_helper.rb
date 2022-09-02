167c5c02affd415f6f81fdf2f0cbf77fb17590a4
Mwagiru <joymwagiru@gmail.com>
2022-09-02 10:50:48 +0300

    Add remote link

--- spec/swagger_helper.rb
+++ spec/swagger_helper.rb
@@ -24,10 +24,10 @@ RSpec.configure do |config|
       paths: {},
       servers: [
         {
-          url: 'http://127.0.0.1:3000/',
+          url: 'https://enventory-management.herokuapp.com/',
           variables: {
             defaultHost: {
-              default: 'http://127.0.0.1:3000/'
+              default: 'https://enventory-management.herokuapp.com/'
             }
           }
         }
